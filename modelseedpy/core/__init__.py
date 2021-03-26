49702591a62a98971e8578d349ff0df69d58aa2c
Christopher Henry <chenry@mcs.anl.gov>
2021-03-26 13:27:29 -0500

    Initial commit of prototype module

---
+++ modelseedpy/core/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from modelseedpy.core.rpcclient import RPCClient
---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='ModelSEEDpy',
+    version='0.1.0',
+    description='Python package for building and analyzing models using ModelSEED',
+    long_description=readme,
+    author='Christopher Henry',
+    author_email='chenry@anl.gov',
+    url='https://github.com/ModelSEED/ModelSEEDpy',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
