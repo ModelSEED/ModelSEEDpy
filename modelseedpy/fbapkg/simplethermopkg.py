a457c6474044282d611ed922b5741fca735efb10
Andrew Freiburger <Andrewefreiburger@gmail.com>
2021-07-21 15:46:22 -0400

    Added logic for parameterized reduction of the constraints

--- modelseedpy/fbapkg/simplethermopkg.py
+++ modelseedpy/fbapkg/simplethermopkg.py
@@ -16,31 +16,43 @@ class SimpleThermoPkg(BaseFBAPkg):
             "filter":None,
             "min_potential":0,
             "max_potential":1000,
-            "dgbin": False
+            "dgbin": False,
+            'reduced_constraints': False
         })
         self.pkgmgr.getpkg("RevBinPkg").build_package(self.parameters["filter"])
         for metabolite in self.model.metabolites:
             self.build_variable(metabolite)
         for reaction in self.model.reactions:
             #Checking that reaction passes input filter if one is provided
-            if self.parameters["filter"] == None or reaction.id in self.parameters["filter"]:
-                if self.parameters['dgbin'] == True:
-                    BaseFBAPkg.build_variable(self,"dgbinF",0,1,"binary",object)
-                    BaseFBAPkg.build_variable(self,"dgbinR",0,1,"binary",object)
-                    
+            if self.parameters["filter"] == None or reaction.id in self.parameters["filter"]:                    
                 self.build_constraint(reaction)
                 
     def build_variable(self,object):
         return BaseFBAPkg.build_variable(self,"potential",self.parameters["min_potential"],self.parameters["max_potential"],"continuous",object)
 
-    def build_constraint(self,object):#Gibbs: dg = Sum(st(i,j)*p(j))
-
+    def build_constraint(self,object):
+        # Gibbs: dg = Sum(st(i,j)*p(j))
         # 0 <= 1000*revbin(i) - 1000*dgbinR + 1000*dgbinF + Sum(st(i,j)*p(j)) <= 1000
-        coef = {self.pkgmgr.getpkg("RevBinPkg").variables["revbin"][object.id] : 1000}
-        if self.parameters['dgbin'] == True:
-            coef[self.variables['dgbinF'][object.id]] = 1000
-            coef[self.variables['dgbinR'][object.id]] = -1000
-        
+        # -1000 <= -1000*revbin + Sum(st(i,j)*p(j)) <= 0
+                              
         for metabolite in object.metabolites:
-            coef[self.variables["potential"][metabolite.id]] = object.metabolites[metabolite]
-        return BaseFBAPkg.build_constraint(self,"thermo",0,1000,coef,object)
+            coef = {self.variables["potential"][metabolite.id]:object.metabolites[metabolite]}
+            
+        if not self.parameters['reduced_constraints']:
+            coef[self.pkgmgr.getpkg("RevBinPkg").variables["revbin"][object.id]] = 1000
+            if self.parameters['dgbin']:
+                # build the dgbin variables
+                BaseFBAPkg.build_variable(self,"dgbinF",0,1,"binary",object)
+                BaseFBAPkg.build_variable(self,"dgbinR",0,1,"binary",object)
+
+                # define the dgbin coefficients
+                coef[self.variables['dgbinF'][object.id]] = 1000
+                coef[self.variables['dgbinR'][object.id]] = -1000            
+            
+            # build the constraint
+            built_constraint = BaseFBAPkg.build_constraint(self,"thermo",0,1000,coef,object)
+            
+        else:
+            built_constraint = None
+                
+        return built_constraint
