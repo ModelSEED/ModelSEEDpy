5d45b7716c1d66552346b7fd7c2cb033c8e62d03
Fxe <fliu@anl.gov>
2021-07-27 18:39:24 -0500

    fixed tests

--- tests/fbapkg/test_revbin_core_model.py
+++ tests/fbapkg/test_revbin_core_model.py
@@ -1,54 +1,48 @@
-# import cobrakbase
-import os
-os.environ["HOME"] = 'C:\\Users\\Andrew Freiburger\\Dropbox\\My PC (DESKTOP-M302P50)\\Documents\\UVic Civil Engineering\\Internships\\Agronne\\cobrakbase'
-import cobrakbase
-token = 'KCYWCM5Q3ZFAGQKDG3ESMJXGHQPK7UMN'
-kbase = cobrakbase.KBaseAPI(token)
-
-# define the example individual model and associated API media package
-model = kbase.get_from_ws('e_coli_core.kb', 95098)
-model.solver = 'optlang-cplex'
-
-# import the modelseedpy packages
+import pytest
+import cobra
+from tests.test_data.mock_data import mock_model_ecoli_core
 from modelseedpy.fbapkg.revbinpkg import RevBinPkg
-revbin = RevBinPkg(model = model)    
 
-# ------------------------ test the RevBin Package ---------------------------------------
-        
-def test_init():
-    
-    # assert results of the model 
-    assert type(revbin.model) is cobrakbase.core.kbasefba.fbamodel.FBAModel
-    assert type(revbin.name) is str
-    assert type(revbin.variable_types) is dict
-    assert type(revbin.constraint_types) is dict
-    assert 'revbin' in list(revbin.variables.keys())
-    assert 'revbinR' in list(revbin.constraints.keys())
-    assert 'revbinF' in list(revbin.constraints.keys())
-    
 
-def test_build_package():
+@pytest.fixture
+def core_model():
+    return mock_model_ecoli_core(True)
+
+
+def test_init(core_model):
+    rev_bin = RevBinPkg(model=core_model)
+    assert type(rev_bin.model) is cobra.core.Model
+    assert type(rev_bin.name) is str
+    assert type(rev_bin.variable_types) is dict
+    assert type(rev_bin.constraint_types) is dict
+    assert 'revbin' in list(rev_bin.variables.keys())
+    assert 'revbinR' in list(rev_bin.constraints.keys())
+    assert 'revbinF' in list(rev_bin.constraints.keys())
+
+
+def test_build_package(core_model):
     # build_variable parameters
     lower_bound = 0
     upper_bound = 1
     var_type = "binary"
     constraint_types = {"F": [None, 0],
                         'R': [None, 1000]}
-    
+
     # execute the function
-    revbin.build_package()
+    rev_bin = RevBinPkg(model=core_model)
+    rev_bin.build_package()
 
     # execute the function and assert results of the function
-    for reaction in revbin.model.reactions:
-        revbin_var = revbin.variables["revbin"][reaction.id]
-        assert revbin_var
-        assert revbin_var.ub == upper_bound
-        assert revbin_var.lb == lower_bound
-        assert revbin_var.type == 'binary'
-        
-        for type in constraint_types:
-            revbin_cons = revbin.constraints['revbin{}'.format(type)][reaction.id]
-            assert revbin_cons
-            assert revbin_cons.lb == constraint_types[type][0]
-            assert revbin_cons.ub == constraint_types[type][1]    
-            assert revbin_cons.name == '{}_revbin{}'.format(reaction.id, type)
+    for reaction in rev_bin.model.reactions:
+        rev_bin_var = rev_bin.variables["revbin"][reaction.id]
+        assert rev_bin_var
+        assert rev_bin_var.ub == upper_bound
+        assert rev_bin_var.lb == lower_bound
+        assert rev_bin_var.type == var_type
+
+        for constraint_type in constraint_types:
+            rev_bin_cons = rev_bin.constraints['revbin{}'.format(constraint_type)][reaction.id]
+            assert rev_bin_cons
+            assert rev_bin_cons.lb == constraint_types[constraint_type][0]
+            assert rev_bin_cons.ub == constraint_types[constraint_type][1]
+            assert rev_bin_cons.name == '{}_revbin{}'.format(reaction.id, constraint_type)
