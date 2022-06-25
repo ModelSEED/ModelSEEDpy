a9ca151e7f0ad377c082518537b645d9f13b3205
Andrew Freiburger <Andrewefreiburger@gmail.com>
2022-06-24 23:16:46 -0400

    exceptions and gapfill edits

---
+++ modelseedpy/core/exceptions.py
@@ -0,0 +1,10 @@
+
+#Adding a few exception classes to handle different types of errors in a central file
+class FeasibilityError(Exception):
+    """Error in FBA formulation"""
+    def __init__(self, message):
+        super(FeasibilityError, self).__init__(message)
+        
+class PackageError(Exception):
+    """Error in package manager"""
+    pass
