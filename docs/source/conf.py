e72710b404a119788272c08b556bfca351fd3f79
Andrew Freiburger <Andrewefreiburger@gmail.com>
2022-07-03 21:42:01 -0400

    comment-out logic for autogeneration of documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,24 @@ master_doc = 'index'
 release = '1'
 version = '0.0.1'
 
-import sys
-from os.path import dirname, join
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
 
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.autosummary",
+#     "sphinx.ext.autodoc",
+#     "sphinx.ext.intersphinx",
+#     "sphinx.ext.mathjax",
+#     "sphinx.ext.viewcode",
+#     "sphinx.ext.napoleon",
+#     "sphinx.ext.autosummary",
     "autoapi",
     "nbsphinx",
 ]
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-SRC_PATH = join(dirname(dirname(__file__)), "src")
-sys.path.insert(0, SRC_PATH)
-autoapi_dirs = [SRC_PATH]
+
+# import sys
+# from os.path import dirname, join
+# SRC_PATH = join(dirname(dirname(__file__)), "src")
+# sys.path.insert(0, SRC_PATH)
+# autoapi_dirs = [SRC_PATH]
