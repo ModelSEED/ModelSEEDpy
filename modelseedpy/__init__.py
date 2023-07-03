97b5d4fe436dc477fefbb451bffbf057660685eb
Christopher Henry <chenry@mcs.anl.gov>
2023-07-03 00:47:30 -0500

    Adding version printing to ModelSEEDpy so I can be sure what version of the code is running

--- modelseedpy/__init__.py
+++ modelseedpy/__init__.py
@@ -11,27 +11,11 @@ from os.path import abspath as _abspath
 from os.path import dirname as _dirname
 from modelseedpy.helpers import config
 
-logging_hash = {
-    "debug": logging.DEBUG,
-    "critical": logging.CRITICAL,
-    "error": logging.ERROR,
-    "warning": logging.WARNING,
-    "info": logging.INFO,
-}
+__author__ = "Christopher Henry"
+__email__ = "chenry@anl.gov"
+__version__ = "0.2.2"
 
-# Configuing modelseedpy logger
-logger = logging.getLogger(__name__)
-c_handler = logging.StreamHandler()
-c_handler.setLevel(logging_hash[config.get("logging", "console_level")])
-c_format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
-c_handler.setFormatter(c_format)
-logger.addHandler(c_handler)
-if config.get("logging", "log_file") == "yes":
-    f_handler = logging.FileHandler(config.get("logging", "filename"), mode="a")
-    f_handler.setLevel(logging_hash[config.get("logging", "file_level")])
-    f_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    f_handler.setFormatter(f_format)
-    logger.addHandler(f_handler)
+print("modelseedpy", __version__)
 
 if sys.version_info[0] == 2:
     logger.warning(
@@ -83,5 +67,3 @@ from modelseedpy.fbapkg import (
 )
 
 from modelseedpy.multiomics import MSExpression
-
-__version__ = "0.2.2"
