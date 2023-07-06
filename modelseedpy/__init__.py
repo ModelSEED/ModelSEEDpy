1851286a03871a758d67e81750dfd4392d6f6da7
Filipe Liu <fliu@anl.gov>
2023-07-06 12:20:51 -0500

    added e0 as extracell search for cobrapy

--- modelseedpy/__init__.py
+++ modelseedpy/__init__.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 # set the warning format to be on a single line
 import sys
 import logging
+import cobra
 import warnings as _warnings
 from os import name as _name
 from os.path import abspath as _abspath
@@ -15,6 +16,8 @@ __author__ = "Christopher Henry"
 __email__ = "chenry@anl.gov"
 __version__ = "0.2.2"
 
+logger = logging.getLogger(__name__)
+
 print("modelseedpy", __version__)
 
 if sys.version_info[0] == 2:
@@ -24,6 +27,9 @@ if sys.version_info[0] == 2:
         "dependencies have already dropped support. At the moment it *should* "
         "still work but we will no longer actively maintain Python 2 support."
     )
+
+if 'e0' not in cobra.medium.annotations.compartment_shortlist['e']:
+    cobra.medium.annotations.compartment_shortlist['e'].append('e0')
 
 import modelseedpy
 from modelseedpy.core import (
